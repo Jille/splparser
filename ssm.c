7ea6e809671fe8fb42aab02fd1c3db302ebdde9c
Sjors Gielen <sjors@sjorsgielen.nl>
2012-05-13 23:19:34 +0200

    Implement get_ssmlabel_from_irlabel, get_ssmlabel_from_irfunc, ssm_iterate_last

--- ssm.c
+++ ssm.c
@@ -1,6 +1,92 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 #include "ssm.h"
 #include "ir.h"
+
+// Ordered mapping of irlabels to ssmlabels
+struct ssmlabelmapping {
+	ssmlabel ssm;
+	irlabel ir;
+	struct ssmlabelmapping *next;
+};
+// Ordered mapping of irfuncs to ssmlabels
+struct ssmfuncmapping {
+	ssmlabel ssm;
+	irfunc ir;
+	struct ssmfuncmapping *next;
+};
+
+static struct ssmlabelmapping *ssmlabels = NULL;
+static struct ssmfuncmapping *ssmfuncs = NULL;
+static ssmlabel ssmlabelptr = 0;
+
+ssmlabel
+get_ssmlabel_from_irlabel(irlabel ir) {
+	if(ssmlabels != NULL && ssmlabels->ir == ir)
+		return ssmlabels->ssm;
+	if(ssmlabels == NULL || ssmlabels->ir > ir) {
+		struct ssmlabelmapping *newfst = malloc(sizeof(struct ssmlabelmapping));
+		newfst->ssm = ++ssmlabelptr;
+		newfst->ir = ir;
+		newfst->next = ssmlabels;
+		ssmlabels = newfst;
+		return ssmlabels->ssm;
+	}
+	// find the first mapping whose irlabel >= ir
+	struct ssmlabelmapping *it = ssmlabels;
+	while(it->next != NULL) {
+		if(it->next->ir == ir)
+			return it->next->ssm;
+		if(it->next->ir > ir)
+			break;
+		it = it->next;
+	}
+	struct ssmlabelmapping *newthis = malloc(sizeof(struct ssmlabelmapping));
+	newthis->ssm = ++ssmlabelptr;
+	newthis->ir = ir;
+	newthis->next = it->next;
+	it->next = newthis;
+	return newthis->ssm;
+}
+
+ssmlabel
+get_ssmlabel_from_irfunc(irfunc ir) {
+	if(ssmfuncs != NULL && ssmfuncs->ir == ir)
+		return ssmfuncs->ssm;
+	if(ssmfuncs == NULL || ssmfuncs->ir > ir) {
+		struct ssmfuncmapping *newfst = malloc(sizeof(struct ssmfuncmapping));
+		newfst->ssm = ++ssmlabelptr;
+		newfst->ir = ir;
+		newfst->next = ssmfuncs;
+		ssmfuncs = newfst;
+		return ssmfuncs->ssm;
+	}
+	// find the first mapping whose irlabel >= ir
+	struct ssmfuncmapping *it = ssmfuncs;
+	while(it->next != NULL) {
+		if(it->next->ir == ir)
+			return it->next->ssm;
+		if(it->next->ir > ir)
+			break;
+		it = it->next;
+	}
+	struct ssmfuncmapping *newthis = malloc(sizeof(struct ssmfuncmapping));
+	newthis->ssm = ++ssmlabelptr;
+	newthis->ir = ir;
+	newthis->next = it->next;
+	it->next = newthis;
+	return newthis->ssm;
+}
+
+struct ssmline *
+ssm_iterate_last(struct ssmline *line) {
+	if(line == NULL)
+		return NULL;
+	while(line->next != NULL)
+		line = line->next;
+	return line;
+}
 
 struct ssmline *
 ir_to_ssm(struct irunit *ir) {
