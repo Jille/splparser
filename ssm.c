59a77dd9754b685d144f92dea9d00f9c5ecd5ae0
Sjors Gielen <sjors@sjorsgielen.nl>
2012-05-13 23:34:06 +0200

    Implemented ir_to_ssm for irunits SEQ and EXP; added stubs for MOVE, JUMP, CJUMP, RET and all expressions.

--- ssm.c
+++ ssm.c
@@ -89,8 +89,78 @@ ssm_iterate_last(struct ssmline *line) {
 }
 
 struct ssmline *
+ir_exp_to_ssm(struct irunit *ir, int reg) {
+	// if reg == 0, throw away result
+	struct ssmline *nop = malloc(sizeof(struct ssmline));
+	nop->label = 0;
+	nop->instr = NOP;
+	nop->next = 0;
+	return nop;
+}
+
+struct ssmline *
 ir_to_ssm(struct irunit *ir) {
-	return 0;
+	assert(ir != NULL);
+
+	struct ssmline *nop = malloc(sizeof(struct ssmline));
+	nop->label = 0;
+	nop->instr = NOP;
+	nop->next = 0;
+
+	switch(ir->type) {
+	case MOVE: // Evaluate src and store the result in dst (TEMP or MEM)
+		assert(ir->move.dst->type == TEMP || ir->move.dst->type == MEM);
+
+		//struct ssmline *ir
+
+		if(ir->move.dst->type == TEMP) {
+			// Store the result in a temporary variable
+			// TODO
+		} else {
+			// Store the result in a memory location
+			// TODO
+		}
+		return nop;
+		/*show_ir_tree(ir->move.dst, indent);
+		show_ir_tree(ir->move.src, indent);
+		break;*/
+	case JUMP: // jump to address as result of expression
+		//show_ir_tree(ir->jump.exp, indent);
+		return nop;
+	case CJUMP: // jump to label as result of relop
+		/*show_ir_tree(ir->cjump.left, indent);
+		printf("%s\n", irop_to_string(ir->cjump.op, 1));
+		show_ir_tree(ir->cjump.right, indent);
+		printf("if true: %d\n", ir->cjump.iftrue);
+		printf("if false: %d\n", ir->cjump.iffalse);
+		break;*/
+		return nop;
+	case SEQ: // Statement "left" followed by "right"
+		// Unless "left" is a label or function declaration, in which
+		// case it simply sets the label of "right"
+		if(ir->seq.left->type == LABEL) {
+			struct ssmline *res = ir_to_ssm(ir->seq.right);
+			res->label = get_ssmlabel_from_irlabel(ir->seq.left->label);
+			return res;
+		} else if(ir->seq.left->type == FUNC) {
+			struct ssmline *res = ir_to_ssm(ir->seq.right);
+			res->label = get_ssmlabel_from_irfunc(ir->seq.left->func);
+			return res;
+		} else {
+			struct ssmline *first = ir_to_ssm(ir->seq.left);
+			struct ssmline *second = ir_to_ssm(ir->seq.right);
+			ssm_iterate_last(first)->next = second;
+			return first;
+		}
+	case RET: // Find the return pointer on the stack, jump to it
+		// TODO
+		return nop;
+	case EXP: // evaluate expression, throw away result
+		return ir_exp_to_ssm(ir->exp, 0);
+	default:
+		printf("Didn't expect IR type %d here\n", ir->type);
+		assert(0 && "Didn't expect that IR type here");
+	}
 }
 
 void
